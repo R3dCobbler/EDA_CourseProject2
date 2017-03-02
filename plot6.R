dc880b9935674be993363a529a326ec17a77551a
Mick Sheahan <R3dCobbler@users.noreply.github.com>
2017-03-02 23:21:02 +0100

    Add files via upload

--- plot6.R
+++ plot6.R
@@ -25,20 +25,19 @@ vehicle.emissions <- merge(vehicle.sources, NEI, by.x = "SCC")
 
 # Identify only results from Baltimore City and Los Angeles County
 
-vehicle.emissions$fips <- as.numeric(vehicle.emissions$fips)
-comp <- subset(vehicle.emissions$fips, 
-               
+comp <- subset(vehicle.emissions, fips == "24510" | fips == "06037")
+comp1 <- aggregate(Emissions ~ year + fips, comp, sum)
 
+# Rename the fips variables for the legend in the plot
 
-baltimore <- subset(vehicle.emissions, fips == "24510")
-LAcounty <- subset(vehicle.emissions, fips == "06037")
-
+comp1$fips <- factor(comp1$fips, levels = c("24510","06037"), labels = c("Baltimore City","Los Angeles County"))
 
 # Create plot
 
 png("plot6.png")
-ggplot() + geom_line(aes(x = factor(year), y = Emissions, colour = fips), data = comp, stat = "identity") +
-  ggtitle("Comparison of pm25 Emissions from Motor Vehicles in Baltimore City and LA County: 1999 - 2008")
+ggplot(comp1, aes(year, Emissions, color = fips)) + geom_line() +
+  ggtitle("Comparison of Motor Vehicle Emissions in Baltimore City and Los Angeles County: 1999-2008") +
+  xlab("Year") + ylab("pm25 Emission Levels") 
 dev.off()
 
 
